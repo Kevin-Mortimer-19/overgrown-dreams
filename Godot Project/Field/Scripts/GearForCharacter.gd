70ffc2b5753a04e905f041c3dfe935924c47c3e4
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2024-05-31 17:18:06 -0400

    created first draft of UI menu

--- Godot Project/Field/Scripts/GearForCharacter.gd
+++ Godot Project/Field/Scripts/GearForCharacter.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var c: CharacterData
+@export var character: CharacterData
 
 var helm_list: Array[Equipment] = []
 var shirt_list: Array[Equipment] = []
@@ -10,6 +10,10 @@ var arms_list: Array[Equipment] = []
 @onready var shirt_list_UI = $VBoxContainer/Shirt/OptionButton
 @onready var arms_list_UI = $VBoxContainer/Arms/OptionButton
 
+func _ready():
+	helm_list_UI.item_selected.connect(helmet_select)
+	shirt_list_UI.item_selected.connect(shirt_select)
+	arms_list_UI.item_selected.connect(arms_select)
 
 func generate_menus():
 	var idx = 0
@@ -27,6 +31,17 @@ func generate_menus():
 		arms_list_UI.add_item(e.equipment_name, idx)
 		arms_list_UI.set_item_metadata(idx, e)
 		idx += 1
+
+func helmet_select(idx: int):
+	get_parent().equipment_swap(character, helm_list_UI.get_item_metadata(idx))
+
+func arms_select(idx:int):
+	get_parent().equipment_swap(character, arms_list_UI.get_item_metadata(idx))
+
+func shirt_select(idx:int):
+	get_parent().equipment_swap(character, shirt_list_UI.get_item_metadata(idx))
+	
+
 
 func add_to_head_list(e:Equipment):
 	helm_list.append(e)
--- Godot Project/Field/Scripts/GearMenu.gd
+++ Godot Project/Field/Scripts/GearMenu.gd
@@ -5,6 +5,8 @@ extends TabContainer
 @export var gear_list: Array[Equipment]
 
 @onready var HeinrichGearMenu = $Heinrich
+@onready var MarsGearMenu = $Mars
+@onready var MedeaGearMenu = $Medea
 
 
 func _ready():
@@ -13,9 +15,32 @@ func _ready():
 		match e.equip_type:
 			Data.GearSlots.HEAD:
 				HeinrichGearMenu.add_to_head_list(e)
+				MarsGearMenu.add_to_head_list(e)
+				MedeaGearMenu.add_to_head_list(e)
 			Data.GearSlots.ARMS:
 				HeinrichGearMenu.add_to_arm_list(e)
+				MarsGearMenu.add_to_arm_list(e)
+				MedeaGearMenu.add_to_arm_list(e)
 			Data.GearSlots.CHEST:
 				HeinrichGearMenu.add_to_chest_list(e)
+				MarsGearMenu.add_to_chest_list(e)
+				MedeaGearMenu.add_to_chest_list(e)
 	HeinrichGearMenu.generate_menus()
-	
+	MarsGearMenu.generate_menus()
+	MedeaGearMenu.generate_menus()
+
+
+func equipment_swap(c: CharacterData, e: Equipment):
+	match e.equip_type:
+			Data.GearSlots.HEAD:
+				c.helm = e
+				equip_test(e.equipment_name, c.stats.actor_name)
+			Data.GearSlots.ARMS:
+				c.gloves = e
+				equip_test(e.equipment_name, c.stats.actor_name)
+			Data.GearSlots.CHEST:
+				c.shirt = e
+				equip_test(e.equipment_name, c.stats.actor_name)
+
+func equip_test(en, cn):
+	print("Equipped the " + en + " on " + cn + ".")
